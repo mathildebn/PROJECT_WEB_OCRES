bc450fc51b841ae8b522b5b011a46acc99d5aec4
mathildebn <47555452+mathildebn@users.noreply.github.com>
2020-11-29 17:58:23 +0100

    Storybook widget Todolist

---
+++ frontend/src/stories/11-Todolist.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Todolist from '../components/Todolist';
+
+export default {
+  title: 'Todolist',
+  component: Todolist,
+};
+
+export const widget_todolist = () => <Todolist> </Todolist>
