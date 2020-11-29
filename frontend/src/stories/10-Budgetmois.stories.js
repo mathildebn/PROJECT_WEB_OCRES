7559c7776ad5ec012b35f98dc647eb8cbc7ac628
mathildebn <47555452+mathildebn@users.noreply.github.com>
2020-11-29 17:50:48 +0100

    Storybook widget Budget par mois

---
+++ frontend/src/stories/10-Budgetmois.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Budgetmois from '../components/Budgetmois';
+
+export default {
+  title: 'Budgetmois',
+  component: Budgetmois,
+};
+
+export const widget_budgetmois = () => <Budgetmois> </Budgetmois>
