532e20421b593f4e77888ed121c79623d038eebc
mathildebn <47555452+mathildebn@users.noreply.github.com>
2020-11-29 17:51:28 +0100

    Delete 5-Budget.stories.js

--- frontend/src/stories/5-Budget.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Budget from '../components/Budget';
-
-export default {
-  title: 'Budget',
-  component: Budget,
-};
-
-export const widget_budget = () => <Budget> </Budget>
