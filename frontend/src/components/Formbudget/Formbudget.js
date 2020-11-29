3ccc6c3e48f36b413eebb6a84b7f4a6d7a4fbf28
Nivine20 <47658193+Nivine20@users.noreply.github.com>
2020-11-29 23:01:16 +0100

    Formbudget new version

--- frontend/src/components/Formbudget/Formbudget.js
+++ frontend/src/components/Formbudget/Formbudget.js
@@ -6,7 +6,7 @@ class Formbudget extends React.Component {
 	render() {
     return (
       <div className="formbudget">
-        <h2 id="titrebud">Budget</h2>
+        <h2 id="titre">Budget</h2>
               <input className="dob" type="text" name="name" placeholder="Type de dépenses" />
               <input className="dob" type="text" name="name" placeholder="Combien ?" />
               <button className="ajout">Ajouter</button>
