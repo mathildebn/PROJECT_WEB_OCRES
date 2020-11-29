c3f0918125777a9d47ff6ffc9fe863e56df5e78b
mathildebn <47555452+mathildebn@users.noreply.github.com>
2020-11-29 17:53:44 +0100

    Storybook widget budget

---
+++ frontend/src/stories/5-Budget.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Budget from '../components/Budget';
+
+export default {
+  title: 'Budget',
+  component: Budget,
+};
+
+export const widget_budget = () => <Budget> </Budget>
